377889671936f07a2fb15b834bc5b875aa695aab
Eduardo Golin Zanela <eduardogzanela@gmail.com>
2021-01-09 11:15:48 -0300

    first commit

---
+++ src/main/java/br/com/eduardozanela/App.java
@@ -0,0 +1,13 @@
+package br.com.eduardozanela;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
